35944264629fc3265666d5d1120c191b6be2b942
Kubiicek <asusdanillo@gmail.com>
2025-07-12 19:03:39 -0300

    :sparkles: add POST endpoint to create new item

--- src/main/java/beginning_of_everything1/springboot/service/AnimeService.java
+++ src/main/java/beginning_of_everything1/springboot/service/AnimeService.java
@@ -1,9 +1,10 @@
 package beginning_of_everything1.springboot.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -11,7 +12,10 @@ import beginning_of_everything1.springboot.domain.Anime;
 
 @Service
 public class AnimeService {
-	private List<Anime> animes = List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Berserk"));
+	private static List<Anime> animes;
+	static {
+		animes = new ArrayList<>(List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Berserk")));
+	}
 	
 	//En: It's almost impossible to have a list because we don't want the user to know the entire database.
 	//Br: quase impossivel ter um listall pois nao queremos que o usuario saiba todo o banco de dados
@@ -26,5 +30,11 @@ public class AnimeService {
 				.filter(anime -> anime.getId().equals(id))
 				.findFirst()
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+	}
+	
+	public Anime save(Anime anime) {
+		anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+		animes.add(anime);
+		return anime;
 	}
 }
