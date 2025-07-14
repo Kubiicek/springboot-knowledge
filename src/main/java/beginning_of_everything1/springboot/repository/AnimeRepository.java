31c2b367e8a98851eef8d4f08fcf5cc3f25904d5
Kubiicek <asusdanillo@gmail.com>
2025-07-14 10:22:14 -0300

    :sparkles: feat: enable anime search by name via GET request param

--- src/main/java/beginning_of_everything1/springboot/repository/AnimeRepository.java
+++ src/main/java/beginning_of_everything1/springboot/repository/AnimeRepository.java
@@ -1,9 +1,12 @@
 package beginning_of_everything1.springboot.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import beginning_of_everything1.springboot.domain.Anime;
 
 public interface AnimeRepository extends JpaRepository<Anime, Long> {
 	
+	List<Anime> findByName(String name);
 }
